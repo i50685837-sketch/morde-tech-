2a1354b3efbc1bd59de33db0a5171ad155f61b27
i50685837-sketch <i50685837@gmail.com>
2026-07-31 10:41:49 +0300

    Create settings.js

---
+++ config/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+    autoRead: true,
+    autoTyping: false,
+    autoStatusView: false,
+
+    welcome: true,
+    antiLink: true,
+    antiSpam: true,
+
+    publicMode: true,
+
+    packname: "Morde-Tech",
+    author: "Mordecai",
+
+    version: "1.0.0"
+};
